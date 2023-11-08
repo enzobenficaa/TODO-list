55a48a083dcabd8397062b6779d96a353f2ca1e2
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-11-08 15:51:59 -0300

    Update task.js

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
 const checklistDependentRoute = express.Router();
+const simpleRouter = express.Router();
 const Checklist = require("../models/checklist");
 const Task = require("../models/task");
 
-checklistDependentRoute.get("/:id/task/new", async (req, res) => {
+checklistDependentRoute.get("/:id/tasks/new", async (req, res) => {
   try {
     let task = Task();
     res
@@ -13,26 +13,59 @@ checklistDependentRoute.get("/:id/task/new", async (req, res) => {
   } catch (error) {
     res
       .status(422)
-      .render("pages/error", { erorrs: "Erro ao carregar o formulário" });
+      .render("pages/error", { errors: "Erro ao carregar o formulário" });
   }
 });
 
-checklistDependentRoute.post("/:id/task", async (req, res) => {
+simpleRouter.delete("/:id", async (req, res) => {
+  try {
+    let task = await Task.findByIdAndDelete(req.params.id);
+    let checklist = await Checklist.findById(task.checklist);
+    let taskToRemove = checklist.tasks.indexOf(task._id);
+    checklist.tasks.slice(taskToRemove, 1);
+    checklist.save();
+    res.redirect(`/checklist/${checklist._id}`);
+  } catch (error) {
+    res
+      .status(422)
+      .render("pages/error", { errors: "Erro ao excluir uma tarefa" });
+  }
+});
+
+checklistDependentRoute.post("/:id/tasks", async (req, res) => {
   let { name } = req.body.task;
   let task = new Task({ name, checklist: req.params.id });
   try {
     await task.save();
-    let checklist = checklist.findById(req.params.id);
-    checklist.task.push(task);
+    let checklist = await Checklist.findById(req.params.id);
+    checklist.tasks.push(task);
     await checklist.save();
-    res.redirect(`/checklists/${req.params.id}`);
+    res.redirect(`/checklist/${req.params.id}`);
   } catch (error) {
     let errors = error.errors;
-    res.status(422).render("task/new", {
-      task: { ...task, errors },
-      checklistId: req.params.id,
-    });
+    res
+      .status(422)
+      .render("tasks/new", {
+        task: { ...task, errors },
+        checklistId: req.params.id,
+      });
   }
 });
 
-module.exports = { checklistDependent: checklistDependentRoute };
+simpleRouter.put("/:id", async (req, res) => {
+  let task = await Task.findById(req.params.id);
+  console.log(task);
+  try {
+    task.set(req.body.task);
+    await task.save();
+    res.status(200).json({ task });
+  } catch (error) {
+    let errors = error.errors;
+    res.status(422).json({ task: { ...errors } });
+  }
+});
+
+module.exports = {
+  checklistDependent: checklistDependentRoute,
+  simple: simpleRouter,
+};
