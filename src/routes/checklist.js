ed21f42ef854584e30077295c3b7caf6fffc6e1b
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-11-08 15:51:28 -0300

    Update checklist.js

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const { populate } = require("../models/routes/checklists");
 const router = express.Router();
-const Checklist = require("../models/checklist");
+const Checklist = require("../models/checklists");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,82 +10,81 @@ router.get("/", async (req, res) => {
     res.status(200).render("checklists/index", { checklists: checklists });
   } catch (error) {
     res
-      .status(200)
+      .status(500)
       .render("pages/error", { error: "Erro ao exibir as listas" });
   }
 });
 
 router.get("/new", async (req, res) => {
   try {
-    let checklist = new Checklist();
-    res.status(200).render("checklists/new", { checklist: checklist });
+    let checklists = new Checklist();
+    res.status(200).render("checklists/new", { checklist: checklists });
   } catch (error) {
     res
       .status(500)
-      .render("pages/error", { errors: "Erro ao carregar formulário" });
+      .render("pages/error", { errors: "Erro ao carregar o formulario" });
   }
 });
 
 router.get("/:id/edit", async (req, res) => {
   try {
-    let checklist = await Checklist.findById(req.params.id);
-    res.status(200).render("checklists/edit", { checklist: checklist });
+    let checklists = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/edit", { checklist: checklists });
   } catch (error) {
     res.status(500).render("pages/error", {
-      error: "Erro ao exibir a edição de listas de tarefas",
+      error: "Erro ao exibir a edição de listas de tarefa",
     });
   }
 });
 
 router.post("/", async (req, res) => {
-  let { name } = req.body.checklist;
-  let checklist = new Checklist({ name });
+  let { name } = req.body.checklists;
+  let checklists = new Checklist({ name });
+
   try {
-    await checklist.save();
+    await checklists.save();
     res.redirect("/checklists");
   } catch (error) {
     res
       .status(422)
-      .render("checklists/new", { checklist: { ...checklist, error } });
+      .render("checklists/new", { checklists: { ...checklists, error } });
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    let checklist = await Checklist.findById(req.params.id).populate("tasks");
-    res.status(200).render("checklists/show", { checklist: checklist });
+    let checklists = await Checklist.findById(req.params.id).populate("tasks");
+    res.status(200).render("checklists/show", { checklist: checklists });
   } catch (error) {
+    console.log(error);
     res
       .status(500)
-      .render("pages/error", { error: "Erro ao exibir as listas" });
+      .render("pages/error", { error: "Erro ao exibir a lista de tarefa" });
   }
 });
 
 router.put("/:id", async (req, res) => {
-  let { name } = req.body.checklist;
-  let checklist = await Checklist.findById(req.params.id);
+  let { name } = req.body.checklists;
+  let checklists = await Checklist.findById(req.params.id);
   try {
-    checklist.name = name;
-    await checklist.save(); // o save serve tanto para criar quanto para atualizar
+    await checklists.update({ name });
     res.redirect("/checklists");
   } catch (error) {
-    console.log(error);
     let errors = error.errors;
     res
       .status(422)
-      .render("checklists/edit", { checklist: { ...checklist, errors } });
+      .render("checklists/edit", { checklist: { ...checklists, errors } });
   }
 });
 
 router.delete("/:id", async (req, res) => {
   try {
-    let { name } = req.body;
-    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    let checklists = await Checklist.findByIdAndRemove(req.params.id);
     res.redirect("/checklists");
   } catch (error) {
     res
       .status(500)
-      .render("pages/error", { error: "Erro ao deletar as listas" });
+      .render("pages/error", { error: "Erro ao deletar a lista de tarefa" });
   }
 });
 
