00a0fe9b61db509d058e76a0965751049d009a0a
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-11-13 14:12:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require("express");
+const checklistRouter = require("./src/routes/checklist");
 const path = require("path");
-const checkListRouter = require("./src/routes/checklist");
+const taskRouter = require("./src/routes/task");
+
 const rootRouter = require("./src/routes/index");
+
 const methodOverride = require("method-override");
-
 require("./config/database");
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
-app.use("/checklists", checkListRouter);
-app.use("/", rootRouter);
 
+app.use("/", rootRouter);
+app.use("/checklists", checklistRouter);
+app.use("/checklists", taskRouter.checklistDependent);
+app.use("/tasks", taskRouter.simple);
 app.listen(3000, () => {
-  console.log("Servidor iniciado");
+  console.log("Servidor ativo!");
 });
